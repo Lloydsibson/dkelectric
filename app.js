8e5a8b891288259225b5a98bc7cb78bfb5ef9160
Lloyd Sibson <lloydsibson@MacBook-Pro-2.local>
2026-03-14 00:54:04 +0000

    Dev: Updated CSS & Removed Unused Photoswipe Files

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 // PHOTOSWIPE GALLERY
+// https://photoswipe.com/
+// https://github.com/dimsemenov/photoswipe?tab=readme-ov-file
+
 // 1. Import the Lightbox logic
-import PhotoSwipeLightbox from "./vendor/photoswipe/photoswipe-lightbox.esm.js";
+import PhotoSwipeLightbox from "./vendor/photoswipe/photoswipe-lightbox.esm.min.js";
 
 // 2. Initialize the gallery
 const lightbox = new PhotoSwipeLightbox({
   gallery: "#photoswipe-gallery", // The container ID
   children: "a", // The clickable elements
   // Dynamically import the UI module when a user clicks an image
-  pswpModule: () => import("./vendor/photoswipe/photoswipe.esm.js"),
+  pswpModule: () => import("./vendor/photoswipe/photoswipe.esm.min.js"),
 });
 
 lightbox.init();
