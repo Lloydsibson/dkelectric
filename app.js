06ceecc37282a20003c3f9bcf82c4273b96dae1a
Lloyd Sibson <lloydsibson@MacBook-Pro-2.local>
2026-03-14 00:47:26 +0000

    Dev: CSS Update

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 // PHOTOSWIPE GALLERY
 // 1. Import the Lightbox logic
-import PhotoSwipeLightbox from "https://unpkg.com/photoswipe/dist/photoswipe-lightbox.esm.js";
+import PhotoSwipeLightbox from "./vendor/photoswipe/photoswipe-lightbox.esm.js";
 
 // 2. Initialize the gallery
 const lightbox = new PhotoSwipeLightbox({
   gallery: "#photoswipe-gallery", // The container ID
   children: "a", // The clickable elements
   // Dynamically import the UI module when a user clicks an image
-  pswpModule: () =>
-    import("https://unpkg.com/photoswipe/dist/photoswipe.esm.js"),
+  pswpModule: () => import("./vendor/photoswipe/photoswipe.esm.js"),
 });
 
 lightbox.init();
